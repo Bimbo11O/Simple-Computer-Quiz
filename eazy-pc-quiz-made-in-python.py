e7f2340a42800bc4cf08aafb14f8a1fc0a453740
Bimbo11O <114340867+Bimbo11O@users.noreply.github.com>
2022-09-25 13:47:58 +0200

    Add files via upload

---
+++ eazy-pc-quiz-made-in-python.py
@@ -0,0 +1,14 @@
+print("Welcome to my Computer Quiz!")
+
+playing = input("Do you want to play my game? ")
+
+print (playing)
+if playing != "yes": quit()
+
+print("AMAZING! Lets play, shall we?...")
+
+answer = input("What does CPU stand for? ")
+
+if answer == "central processing unit":
+ 
+ print("!CORRECT!")
